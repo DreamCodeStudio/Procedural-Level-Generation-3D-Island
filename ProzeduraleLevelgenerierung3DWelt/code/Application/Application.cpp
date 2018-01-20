a61bc01a86337bd375a2449a0e29db80dd55a08d
Michael Hollmer <michaelhollmer@freenet.de>
2018-01-21 00:57:24 +0100

    Added Ocean around the generated island

--- ProzeduraleLevelgenerierung3DWelt/code/Application/Application.cpp
+++ ProzeduraleLevelgenerierung3DWelt/code/Application/Application.cpp
@@ -8,14 +8,22 @@ void Application::Create()
 	_Manager = _Device->getSceneManager();		//Get scene manager
 
 	/* Create camera */
-	_Camera = _Manager->addCameraSceneNodeFPS();
-	_Camera->setFarValue(1000000);
+	_Camera = _Manager->addCameraSceneNodeMaya(0, -1500, 200, 1500, -1, 5000, true);
+	_Camera->setFarValue(500000);
+	_Camera->setTarget(irr::core::vector3df(0, 50, 0));
 
 	/* Create Heightmap */
-	WorldGenerator::Create(1213, 1729, "Heightmap.png", 150);
+	WorldGenerator::Create(2000, 2000, "Heightmap.png", 190);
 
-	_Terrain = _Manager->addTerrainSceneNode("Heightmap.png", 0, -1, irr::core::vector3df(0, 0, 0), irr::core::vector3df(0, 0, 0), irr::core::vector3df(3, 5, 3));
-	_Light = _Manager->addLightSceneNode(0, irr::core::vector3df(0, 1000, 0), irr::video::SColor(255, 255, 255, 255), 2000, -1);
+	_Terrain = _Manager->addTerrainSceneNode("Heightmap.png", 0, -1, irr::core::vector3df(0, 0, 0), irr::core::vector3df(0, 0, 0), irr::core::vector3df(3, 6, 3), irr::video::SColor(255, 255, 255, 255), 5, irr::scene::E_TERRAIN_PATCH_SIZE::ETPS_129, 10, false);
+	_Terrain->setPosition(irr::core::vector3df(_Terrain->getPosition().X + (_Terrain->getPosition().X - _Terrain->getBoundingBox().getCenter().X), 0, _Terrain->getPosition().Z + (_Terrain->getPosition().Z - _Terrain->getBoundingBox().getCenter().Z)));
+	_Light = _Manager->addLightSceneNode(0, irr::core::vector3df(200, 1000, 200), irr::video::SColor(255, 255, 255, 255), 5000, -1);
+
+	/* Create ocean */
+	_HillMesh = _Manager->addHillPlaneMesh("HillMesh", irr::core::dimension2d<irr::f32>(500, 500), irr::core::dimension2d<irr::u32>(200, 200), 0, 0, irr::core::dimension2d<irr::f32>(0, 0));
+	_WaterSurface = _Manager->addWaterSurfaceSceneNode(_HillMesh, 50.0, 800, 100, 0, -1, irr::core::vector3df(0, 0, 0), irr::core::vector3df(0, 0, 0), irr::core::vector3df(1, 1, 1));
+	_WaterSurface->setMaterialTexture(0, _Driver->getTexture("water_texture.jpg"));
+	_WaterSurface->setPosition(irr::core::vector3df(_WaterSurface->getPosition().X + (_WaterSurface->getPosition().X - _WaterSurface->getBoundingBox().getCenter().X), 10, _WaterSurface->getPosition().Z - (_WaterSurface->getPosition().Z - _WaterSurface->getBoundingBox().getCenter().Z)));
 }
 
 void Application::Run()
@@ -25,6 +33,7 @@ void Application::Run()
 		_Driver->beginScene(true, true, irr::video::SColor(100, 100, 100, 100));
 
 		_Manager->drawAll();
+		_Light->setPosition(_Camera->getPosition());
 
 		_Driver->endScene();
 	}
--- ProzeduraleLevelgenerierung3DWelt/code/Application/Application.h
+++ ProzeduraleLevelgenerierung3DWelt/code/Application/Application.h
@@ -4,6 +4,9 @@
 /* WorldGenerator */
 #include "WorldGenerator\WorldGenerator.h"
 
+/* For loading config file */
+#include <fstream>
+
 class Application
 {
 	public:
@@ -23,4 +26,29 @@ class Application
 		irr::scene::ICameraSceneNode *_Camera;
 		irr::scene::ITerrainSceneNode *_Terrain;
 		irr::scene::ILightSceneNode *_Light;
+		irr::scene::IAnimatedMesh *_HillMesh;
+		irr::scene::ISceneNode *_WaterSurface;
+
+		/* Config */
+		
+		/* Water */
+		int _WaterTileSizeX, _WaterTileSizeY;
+		int _WaterTileAmountX, _WaterTileAmountY;
+		int _WaterScaleX, _WaterScaleY;
+		int _WaterWaveLength, _WaterWaveHeight, _WaterWaveSpeed;
+
+		/* Camera */
+		float _CameraTargetX, _CameraTargetY, _CameraTargetZ;
+		float _CameraDistance;
+
+		/* Light */
+		int _LightRadius;
+
+		/* World */
+		int _WorldMapSizeX, _WorldMapSizeY;
+		int _WorldScaleX, _WorldScaleY, _WorldScaleZ;
+		int _WorldPeakDistance;
+		int _WorldSmoothFactor;
+		
+
 };
